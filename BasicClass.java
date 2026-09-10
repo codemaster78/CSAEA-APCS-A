56cf3d12b0961362832519117dc63b212a774cc4
Solidbeast42 <67381170+codemaster78@users.noreply.github.com>
2026-09-10 13:09:03 +0000

    OOP Framework start

---
+++ BasicClass.java
@@ -0,0 +1,15 @@
+public class BasicClass{
+    public static void main(String[] args) {
+
+        // Classes
+            // Attributes/data
+            // Variables
+
+            // Constructor - Special method called every time you make an object
+            // Same name as the class, followed by paranthesis
+            // BasicClass()
+
+            // Behaviors/Methods
+            // You can write as many as you want
+    }
+}
